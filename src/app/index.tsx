d61eca92672e251fcf015f5b3a066b41b71a4dfb
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-07-14 18:23:31 -0300

    creating target component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,22 @@
 import { View } from "react-native";
 
 import { HomeHeader } from "@/components/home-header";
+import { Target } from "@/components/target";
 
 const summary = {
   total: "R$2.680,00",
   input: { label: "Entradas", value: "R$6.184,90" },
   output: { label: "Saídas", value: "-R$883,65" },
 };
+
+const targets = [
+  {
+    name: "Comprar uma cadeira ergonômica",
+    percentage: "75%",
+    current: "900,00",
+    target: "1.200,00",
+  },
+];
 
 export default function Index() {
   return (
@@ -16,6 +26,7 @@ export default function Index() {
       }}
     >
       <HomeHeader data={summary} />
+      <Target data={targets[0]} />
     </View>
   );
 }
