11166253ab6b2988b09c93de317f5dbbd4f683ae
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-07-15 21:12:09 -0300

    creating currency input

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,6 +1,7 @@
 import { StatusBar, View } from "react-native";
 
 import { Button } from "@/components/button";
+import { CurrencyInput } from "@/components/currency-input";
 import { Input } from "@/components/input";
 import { PageHeader } from "@/components/page-header";
 
@@ -24,6 +25,9 @@ export default function Target() {
           label="Nova meta"
           placeholder="Ex: Viagem para praia, Apple Watch"
         />
+
+        <CurrencyInput label="Valor alvo" value={24555.93} />
+
         <Button title="Salvar" />
       </View>
     </View>
---
+++ src/components/currency-input/index.tsx
@@ -0,0 +1,29 @@
+import { Text, View } from "react-native";
+import Input, { CurrencyInputProps } from "react-native-currency-input";
+
+import { colors } from "@/theme";
+
+import { styles } from "@/components/input/styles";
+
+type InputProps = CurrencyInputProps & {
+  label: string;
+};
+
+export function CurrencyInput({ label, ...rest }: InputProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+
+      <Input
+        style={styles.input}
+        placeholderTextColor={colors.gray[400]}
+        prefix="R$ "
+        delimiter="."
+        separator=","
+        precision={2}
+        minValue={0}
+        {...rest}
+      />
+    </View>
+  );
+}
