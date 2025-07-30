724db85298a59eb49f73f498cf63cc596715b2cd
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2025-07-30 18:54:13 -0300

    creating method create for transaction hook

--- src/database/useTargetDatabase.ts
+++ src/database/useTargetDatabase.ts
@@ -23,9 +23,12 @@ export function useTargetDatabase() {
   const database = useSQLiteContext();
 
   async function create(data: TargetCreate) {
-    const statement = await database.prepareAsync(
-      "INSERT INTO targets (name, amount) values ($name, $amount)"
-    );
+    const statement = await database.prepareAsync(`
+      INSERT INTO targets 
+        (name, amount) 
+      VALUES 
+        ($name, $amount)
+    `);
 
     statement.executeAsync({
       $name: data.name,
